ab83efef1f3a6b49225109a4bd48a7b299249762
Fisher2911 <fisher1205l@gmail.com>
2022-07-02 23:50:14 -0400

    Added player collection saving/loading

--- src/main/java/io/github/fisher2911/skyblocklevels/listener/PlayerJoinListener.java
+++ src/main/java/io/github/fisher2911/skyblocklevels/listener/PlayerJoinListener.java
@@ -1,17 +1,12 @@
 package io.github.fisher2911.skyblocklevels.listener;
 
 import io.github.fisher2911.skyblocklevels.packet.PacketHelper;
-import io.github.fisher2911.skyblocklevels.user.BukkitUser;
-import io.github.fisher2911.skyblocklevels.user.Collection;
-import io.github.fisher2911.skyblocklevels.user.Cooldowns;
 import io.github.fisher2911.skyblocklevels.user.UserManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-
-import java.util.HashMap;
 
 public class PlayerJoinListener implements Listener {
 
